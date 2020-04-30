ea7de39892e03ed16a6d7de803a9e21058304738
P D K Pavan Kumar <pavankumar8545@gmail.com>
2020-04-30 23:33:34 +0530

    optimizing

--- src/random/threeSum.js
+++ src/random/threeSum.js
@@ -11,26 +11,17 @@ Output: [0, -1, 1], [2, -3, 1]
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-    var arr = [];
+    var arr = {};
     nums = nums.sort((a,b) => a-b);
     for(var i=0;i < nums.length - 2; i++) {
         for(var j=i+1; j< nums.length - 1; j++) {
             for(var k=j+1; k < nums.length; k++) {
                 if(nums[i]+nums[j]+nums[k] === 0) {
                     var result = [nums[i], nums[j], nums[k]].sort((a,b) => a-b);
-                    var exists = false;
-                    for(var a=0; a<arr.length; a++) {
-                        if(arr[a].join("") === result.join("")){
-                            exists = true;
-                            break;
-                        }
-                    }
-                    if(!exists){
-                        arr.push(result);   
-                    }
+                    arr[result.join("")] = result;
                 }
             }
         }
     }
-    return arr;
+    return Object.values(arr);
 };
