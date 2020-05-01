6e995d74ddcdec528f057ce44c6a6873086f7f27
P D K Pavan Kumar <pavankumar8545@gmail.com>
2020-05-01 22:47:12 +0530

    solution to first bad version

---
+++ src/mayChallenge/firstBadVersion.js
@@ -0,0 +1,59 @@
+/**
+  You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
+
+  Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
+
+  You are given an API bool isBadVersion(version) which will return whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.
+
+  Example:
+
+  Given n = 5, and version = 4 is the first bad version.
+
+  call isBadVersion(3) -> false
+  call isBadVersion(5) -> true
+  call isBadVersion(4) -> true
+
+  Then 4 is the first bad version. 
+**/
+
+/**
+ * Definition for isBadVersion()
+ * 
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function(isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function(n) {
+        var left = 1;
+        var right = n;
+        while(true) {
+            var middle = Math.round((left+right)/2);
+            var leftValue = isBadVersion(left);
+            var middleValue = isBadVersion(middle);
+            if(leftValue) {
+                return left;
+            } else {
+                if(middleValue) {
+                    right = middle;
+                    left++;
+                } else {
+                    left = middle;
+                    left++;
+                }
+            }
+            
+        }
+    };
+};
