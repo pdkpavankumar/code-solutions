0cf2c62e5f51e1adf35d167884306f6c55d38128
P D K Pavan Kumar <pavankumar8545@gmail.com>
2020-05-07 22:44:11 +0530

    Create isCousins.js

---
+++ src/mayChallenge/isCousins.js
@@ -0,0 +1,66 @@
+/**
+  In a binary tree, the root node is at depth 0, and children of each depth k node are at depth k+1.
+
+  Two nodes of a binary tree are cousins if they have the same depth, but have different parents.
+
+  We are given the root of a binary tree with unique values, and the values x and y of two different nodes in the tree.
+
+  Return true if and only if the nodes corresponding to the values x and y are cousins.
+
+  Example 1:
+
+  Input: root = [1,2,3,4], x = 4, y = 3
+  Output: false
+
+  Example 2:
+
+  Input: root = [1,2,3,null,4,null,5], x = 5, y = 4
+  Output: true
+
+  Example 3:
+
+  Input: root = [1,2,3,null,4], x = 2, y = 3
+  Output: false
+
+  Note:
+
+  The number of nodes in the tree will be between 2 and 100.
+  Each node has a unique integer value from 1 to 100.
+**/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} x
+ * @param {number} y
+ * @return {boolean}
+ */
+var isCousins = function(root, x, y) {
+    var [xparent, xlevel] = getParentAndLevel(root, x);
+    var [yparent, ylevel] = getParentAndLevel(root, y);
+    return xlevel===ylevel && xparent !== yparent;
+};
+
+var getParentAndLevel = (root, x, parent = 0, level = 0) => {
+        if (!root || root.val === 0) {
+            return [0, 0];
+        }
+        if (root.val === x) {
+            return [parent, level];
+        } else {
+            var a = getParentAndLevel(root.left, x, root.val, level + 1);
+            var b = getParentAndLevel(root.right, x, root.val, level + 1);
+            if(a[0] !== 0) {
+                return a;
+            } else {
+                return b;
+            }
+        }
+}
