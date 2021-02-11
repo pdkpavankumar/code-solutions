a26f166a7e1c28c7079816874c346c2aa51ccaf7
P D K Pavan Kumar <pavankumar8545@gmail.com>
2021-02-11 18:05:50 +0530

    Create isAnagram.js

---
+++ src/2021/febChallenge/isAnagram.js
@@ -0,0 +1,29 @@
+/**
+    Given two strings s and t , write a function to determine if t is an anagram of s.
+
+    Example 1:
+
+    Input: s = "anagram", t = "nagaram"
+    Output: true
+    Example 2:
+
+    Input: s = "rat", t = "car"
+    Output: false
+    Note:
+    You may assume the string contains only lowercase alphabets.
+
+    Follow up:
+    What if the inputs contain unicode characters? How would you adapt your solution to such case?
+**/
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    if(s.length !== t.length) {return false}
+    for(var i=0; i<t.length; i++) {
+        s = s.replace(t.charAt(i)+'', '');
+    }
+    return s === '';
+};
