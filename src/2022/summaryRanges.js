3d1b513d61b0068ceea3690f31497d19b944b1ca
P D K Pavan Kumar <pavankumar8545@gmail.com>
2022-02-28 15:06:43 +0530

    Create summaryRanges.js

---
+++ src/2022/summaryRanges.js
@@ -0,0 +1,66 @@
+/**
+  You are given a sorted unique integer array nums.
+
+  Return the smallest sorted list of ranges that cover all the numbers in the array exactly. 
+  That is, each element of nums is covered by exactly one of the ranges, and there is no integer x such that x is in one of the ranges but not in nums.
+
+  Each range [a,b] in the list should be output as:
+
+  "a->b" if a != b
+  "a" if a == b
+
+  Example 1:
+  Input: nums = [0,1,2,4,5,7]
+  Output: ["0->2","4->5","7"]
+  Explanation: The ranges are:
+  [0,2] --> "0->2"
+  [4,5] --> "4->5"
+  [7,7] --> "7"
+  
+  Example 2:
+  Input: nums = [0,2,3,4,6,8,9]
+  Output: ["0","2->4","6","8->9"]
+  Explanation: The ranges are:
+  [0,0] --> "0"
+  [2,4] --> "2->4"
+  [6,6] --> "6"
+  [8,9] --> "8->9"
+
+  Constraints:
+  0 <= nums.length <= 20
+  -231 <= nums[i] <= 231 - 1
+  All the values of nums are unique.
+  nums is sorted in ascending order.
+**/
+/**
+ * @param {number[]} nums
+ * @return {string[]}
+ */
+var summaryRanges = function(nums) {
+    var start;
+    var current;
+    var result = [];
+    for(var i=0; i<nums.length; i++) {
+        if(start == undefined) {
+            start = nums[i];
+        }else if(nums[i] === nums[i-1] + 1) {
+                 current = nums[i];
+        } else if(start !== current && current!== undefined) {
+            result.push(`${start}->${current}`);
+            start = undefined;
+            i--;
+            current = undefined;
+        } else {
+            result.push(`${start}`);
+            start = undefined;
+            current = undefined;
+            i--;
+        }         
+    }
+    if(start !== undefined && current !== undefined) {
+        result.push(`${start}->${current}`);
+    } else if(start !== undefined) {
+        result.push(`${start}`);
+    }
+    return result;
+};
