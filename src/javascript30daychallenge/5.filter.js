c570aaf568af53e083d3f33c4b493b2cf9455ec6
P D K Pavan Kumar <pavankumar8545@gmail.com>
2023-05-19 21:40:27 +0530

    Create 5.filter.js

---
+++ src/javascript30daychallenge/5.filter.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function(arr, fn) {
+    var newArr = [];
+    for(var i=0; i<arr.length; i++) {
+        if(fn(arr[i], i)) {
+            newArr.push(arr[i]);
+        }
+    }
+    return newArr;
+};
